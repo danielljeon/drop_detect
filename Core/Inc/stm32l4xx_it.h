a0a71b22745615ee6bbf7d112fd853b18ca41a1a
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2025-10-20 13:13:17 -0400

    Initial FreeRTOS implementation

--- Core/Inc/stm32l4xx_it.h
+++ Core/Inc/stm32l4xx_it.h
@@ -51,11 +51,9 @@ void HardFault_Handler(void);
 void MemManage_Handler(void);
 void BusFault_Handler(void);
 void UsageFault_Handler(void);
-void SVC_Handler(void);
 void DebugMon_Handler(void);
-void PendSV_Handler(void);
-void SysTick_Handler(void);
 void EXTI0_IRQHandler(void);
+void TIM1_UP_TIM16_IRQHandler(void);
 void DMA2_Channel3_IRQHandler(void);
 void DMA2_Channel4_IRQHandler(void);
 /* USER CODE BEGIN EFP */
